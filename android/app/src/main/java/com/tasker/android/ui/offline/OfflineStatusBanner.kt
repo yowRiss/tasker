f36f7f4670ca60fd10290929655c2f214466df1e
yowRiss <mnabilfaris91@gmail.com>
2026-08-26 14:38:21 +0800

    fix(android): remove invalid Compose weight imports

--- android/app/src/main/java/com/tasker/android/ui/offline/OfflineStatusBanner.kt
+++ android/app/src/main/java/com/tasker/android/ui/offline/OfflineStatusBanner.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ChevronRight
--- android/app/src/main/java/com/tasker/android/ui/tasks/TaskDatabaseViews.kt
+++ android/app/src/main/java/com/tasker/android/ui/tasks/TaskDatabaseViews.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
